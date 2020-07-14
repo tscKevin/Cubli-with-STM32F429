d257d8a62be0153c5e5463509818b21d75216cdb
Tseng,Shih-Che_Evan <xo3374ox@gmail.com>
2020-07-14 20:02:37 +0800

    Initial test
    LED toggle Test

--- Cubli/User/src/main.c
+++ Cubli/User/src/main.c
@@ -1,5 +1,21 @@
+#include "stm32f4xx.h"
+void main(void){
+   GPIO_InitTypeDef GPIO_InitStructure;
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG,ENABLE); //enable GPIOA clock   //
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOG, &GPIO_InitStructure);
+  while(1){
+    //GPIO_SetBits(GPIOG,GPIO_Pin_13);
+    //GPIO_SetBits(GPIOG,GPIO_Pin_14);
+    
+    GPIO_ToggleBits(GPIOG, GPIO_Pin_13);
+    //GPIO_ToggleBits(GPIOG, GPIO_Pin_14);
 
-int main()
-{
-  return 0;
+    for(int i=0; i<25000000; i++);
+  }
+
 }
--- Cubli/main.c
+++
@@ -1,6 +0,0 @@
-#include "stm32f4xx.h"
-void main(void){
-  printf("Start Cubli by STM32F429ZI");
-  while(1){
-  }
-}
